0db9c8ebfc2db1989f439cf2a4cd77e7ea9ccecb
Filip Nowakowski <filip.nowakowski@monterail.com>
2023-05-14 20:50:21 +0200

    init

---
+++ pages/gallery.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <h2>Events</h2>
+    <div v-for="show in (data as any).events.rows" class="card">
+      <img :src="show.event.coverPhotoPath" />
+      <div>{{ show.event.eventName }}</div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const params = {
+  storeIds: ['115760214753254'],
+  currentStatuses: ['vod', 'live', 'planned'],
+  page: 1,
+  size: 10,
+  skipAggregations: true,
+  onsiteMode: true,
+  platform: 'on_site',
+  sort: {
+    field: 'naturalSorting',
+    direction: 'asc',
+  },
+  showExpired: true,
+};
+
+const { data } = useFetch('https://europe-west1-elisashop-a7b5f.cloudfunctions.net/shopApi/search/liveEvents', {
+  method: 'post',
+  body: params,
+  headers: { 'Content-Type': 'application/json' },
+});
+</script>
+
+<style lang="scss" scoped>
+.card {
+  width: 364px;
+  height: 224px;
+  position: relative;
+  margin-bottom: 12px;
+
+  img {
+    width: 100%;
+    height: auto;
+    max-height: 100%;
+    object-fit: cover;
+  }
+
+  div {
+    position: absolute;
+    width: 100%;
+    bottom: 0;
+    background-color: rgba(255, 255, 255, 0.4);
+  }
+}
+</style>
